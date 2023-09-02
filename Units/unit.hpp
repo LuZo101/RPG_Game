844dda03b75eec743c8a8f14ea5e7ed160147708
LuZo <lukas.zorn@protonmail.com>
2023-09-02 17:28:56 +0200

    modified:   Units/unit.hpp

--- Units/unit.hpp
+++ Units/unit.hpp
@@ -1,5 +1,32 @@
 #ifndef UNIT_HPP
 #define UNIT_HPP
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Unit
+{
+private:
+    string Type;
+    int HP;
+    int Moves;
+    
+
+
+public:
+    Unit(/* args */);
+    ~Unit();
+};
+
+Unit::Unit(/* args */)
+{
+}
+
+Unit::~Unit()
+{
+}
 
 
 
