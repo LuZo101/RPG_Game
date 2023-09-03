73b24b5d0b22094ed8658ddb18aa448796504b4b
LuZo <lukas.zorn@protonmail.com>
2023-09-03 21:47:33 +0200

    modified:   CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
-#include "Units/unit.hpp"
+#include <wx/wx.h>
 
+class HelloWorldApp : public wxApp
+{
+public:
+    virtual bool OnInit() override;
+};
 
-int main(){
-    std::cout<<"hEY sUSE"<<std::endl;
-    return 0;}
+class HelloWorldFrame : public wxFrame
+{
+public:
+    HelloWorldFrame(const wxString& title);
+};
+
+bool HelloWorldApp::OnInit()
+{
+    HelloWorldFrame* frame = new HelloWorldFrame("Hello World in wxWidgets");
+    frame->Show(true);
+    return true;
+}
+
+HelloWorldFrame::HelloWorldFrame(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(300, 200))
+{
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
+    wxStaticText* text = new wxStaticText(panel, wxID_ANY, "Hello, World!", wxPoint(50, 50));
+}
+
+wxIMPLEMENT_APP(HelloWorldApp);
